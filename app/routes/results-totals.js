41c0255472ac57272779f683d8571cbc314591c3
Willtar13 <willsea80@gmail.com>
2020-01-26 05:21:09 -0600

    Added hardcoded total

--- app/routes/results-totals.js
+++ app/routes/results-totals.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class ResultsTotalsRoute extends Route {
+
+    async model () {
+        
+        let response = await fetch('http://localhost:8081/api/totalResults').then(response => response.json());
+
+        return response;
+    }
+
 }
